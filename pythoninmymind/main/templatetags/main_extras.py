7134bf5140b8cace70e45c32b5567bb09dd2a9df
s0meone-s0mewhere <164100527+s0meone-s0mewhere@users.noreply.github.com>
2024-04-12 21:50:13 +0300

    added tables, description for pages and titles

--- pythoninmymind/main/templatetags/main_extras.py
+++ pythoninmymind/main/templatetags/main_extras.py
@@ -1,6 +1,5 @@
 import markdown
-from markdown.extensions.codehilite import CodeHiliteExtension
-from main.markdown_extensions import SlugFieldExtension, ImgStyleExtension
+from main.markdown_extensions import SlugFieldExtension, ImgStyleExtension, TablesExtension
 from django import template
 from django.template.defaultfilters import stringfilter
 from django.utils.safestring import mark_safe
@@ -10,5 +9,5 @@ register = template.Library()
 @register.filter
 @stringfilter
 def render_markdown(value):
-    md = markdown.Markdown(extensions=["fenced_code", SlugFieldExtension(), ImgStyleExtension()])
+    md = markdown.Markdown(extensions=["fenced_code", SlugFieldExtension(), ImgStyleExtension(), TablesExtension(),])
     return mark_safe(md.convert(value))
