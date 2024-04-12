051d653c0f459dc78408c269fd258eb717b7eec6
s0meone-s0mewhere <164100527+s0meone-s0mewhere@users.noreply.github.com>
2024-04-12 21:50:13 +0300

    added tables, description for pages and titles

--- pythoninmymind/main/markdown_extensions.py
+++ pythoninmymind/main/markdown_extensions.py
@@ -1,10 +1,10 @@
 import markdown
 import re
 from django.urls import reverse
-from markdown.inlinepatterns import LinkInlineProcessor, LINK_RE, ImageInlineProcessor, IMAGE_LINK_RE 
-from markdown.extensions import Extension
-from markdown.preprocessors import Preprocessor
+from markdown.inlinepatterns import LinkInlineProcessor, LINK_RE, ImageInlineProcessor, IMAGE_LINK_RE
+from markdown.extensions.tables import TableProcessor
 import xml.etree.ElementTree as etree
+from typing import Sequence
 
 
 
@@ -53,3 +53,64 @@ class ImgStyleExtension(markdown.Extension):
             ImgStyleProcessor(IMAGE_LINK_RE, md), "image-style", 160
         )
     
+
+class TablesProcessor(TableProcessor):
+    def _build_row(self, row: str, parent: etree.Element, align: Sequence[str | None]) -> None:
+            tr = etree.SubElement(parent, 'tr')
+            tag = 'td'
+            if parent.tag == 'thead':
+                tag = 'th'
+            cells = self._split_row(row)
+
+            for i, a in enumerate(align):
+                c = etree.SubElement(tr, tag)
+                if tag == 'th':
+                    c.set("class", "col")
+                    
+                try:
+                    c.text = cells[i].strip(' ')
+                except IndexError: 
+                    c.text = ""
+                if a:
+                    if self.config['use_align_attribute']:
+                        c.set('align', a)
+                    else:
+                        c.set('style', f'text-align: {a};')
+
+
+    def run(self, parent: etree.Element, blocks: list[str]) -> None:
+        block = blocks.pop(0).split('\n')
+        header = block[0].strip(' ')
+        rows = [] if len(block) < 3 else block[2:]
+
+        align: list[str | None] = []
+        for c in self.separator:
+            c = c.strip(' ')
+            if c.startswith(':') and c.endswith(':'):
+                align.append('center')
+            elif c.startswith(':'):
+                align.append('left')
+            elif c.endswith(':'):
+                align.append('right')
+            else:
+                align.append(None)
+
+        div = etree.SubElement(parent, 'div')
+        div.set("class", "overflow-x-auto")
+        table = etree.SubElement(div, 'table')
+        table.set("class", "table")
+        thead = etree.SubElement(table, 'thead')
+        self._build_row(header, thead, align)
+        tbody = etree.SubElement(table, 'tbody')
+        if len(rows) == 0:
+            self._build_empty_row(tbody, align)
+        else:
+            for row in rows:
+                self._build_row(row.strip(' '), tbody, align)
+
+class TablesExtension(markdown.Extension):
+    def extendMarkdown(self, md):
+        if '|' not in md.ESCAPED_CHARS:
+            md.ESCAPED_CHARS.append('|')
+        processor = TablesProcessor(md.parser, self.getConfigs())
+        md.parser.blockprocessors.register(processor, 'table', 75)
